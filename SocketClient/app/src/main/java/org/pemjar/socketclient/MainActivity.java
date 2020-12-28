aeafb8c00b52c125b8ef7ca12db4e5f29d37e4b7
nuruslaily <nuruslaily88@gmail.com>
2020-12-28 13:37:04 +0700

    permission

--- SocketClient/app/src/main/java/org/pemjar/socketclient/MainActivity.java
+++ SocketClient/app/src/main/java/org/pemjar/socketclient/MainActivity.java
@@ -1,11 +1,14 @@
 package org.pemjar.socketclient;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -35,6 +38,11 @@ public class MainActivity extends Activity {
     String SERVER_IP;
     int SERVER_PORT;
     private static final int REQUEST_PATH = 1;
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
     String isi = "";
     int serverResponseCode = 0;
     ProgressDialog dialog = null;
@@ -237,6 +245,19 @@ public class MainActivity extends Activity {
             dialog.dismiss();
             return serverResponseCode;
 
+        }
+    }
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
         }
     }
     private PrintWriter output;
